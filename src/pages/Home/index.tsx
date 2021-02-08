ebc6bdae3a532661f8e8d445959313007dbf710f
jonjau <jonjau3Q@gmail.com>
2021-02-08 13:35:39 +1100

    changed type scale, fleshed out title hero on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,22 @@
+import Hero from "../../components/Hero/Hero";
 import styles from "./Home.module.scss";
+
+const Schedule = () => {
+  return <div className={styles.schedule}>Sched</div>;
+};
 
 const Home = () => {
   return (
-    <div className={styles.home}>
-      <div className={styles.hero}>
-        Hero
+    <>
+      <div className={styles.home}>
+        <div className={styles.hero}>
+          <Hero />
+        </div>
+        <div className={styles.scroll}>Scroll</div>
       </div>
-      <div className={styles.scroll}>
-        Scroll
-      </div>
-    </div>
+      <Schedule />
+    </>
   );
-}
+};
 
 export default Home;
