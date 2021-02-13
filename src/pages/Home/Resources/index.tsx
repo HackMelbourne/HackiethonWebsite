70b45596b7a4bd7884b8ef63961fde379d2355ff
jonjau <jonjau3Q@gmail.com>
2021-02-13 22:49:14 +1100

    refactored button CSS class and changed colours in Resources section

--- src/pages/Home/Resources/index.tsx
+++ src/pages/Home/Resources/index.tsx
@@ -3,41 +3,47 @@ import astronaut1 from "../../../assets/astronaut-participants.svg";
 import astronaut2 from "../../../assets/astronaut-mentors.svg";
 import astronaut3 from "../../../assets/astronaut-judges.svg";
 
+interface Props {
+  title: string;
+  description: string;
+  img: JSX.Element;
+  // ^ almost the same as React.ReactNode except it doesn't allow
+  // plain strings/numbers, only JSX elements
+}
+
+const Pack = ({ title, description, img }: Props) => {
+  return (
+    <div className={styles.pack}>
+      <h3>{title}</h3>
+      <h4>{description}</h4>
+      <div className={styles.imgContainer}>{img}</div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.button}>DOWNLOAD</button>
+      </div>
+    </div>
+  );
+};
+
 const Resources = () => {
   return (
     <section id="resources" className={styles.resources}>
       <h2>Resources</h2>
       <div className={styles.packs}>
-        <div className={styles.pack}>
-          <h3>Participants</h3>
-          <h4>Participants Resources Pack</h4>
-          <div className={styles.imgContainer}>
-            <img src={astronaut1} alt="blue astronaut face" />
-          </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.button}>DOWNLOAD</button>
-          </div>
-        </div>
-        <div className={styles.pack}>
-          <h3>Mentors</h3>
-          <h4>Mentors Resources Pack</h4>
-          <div className={styles.imgContainer}>
-            <img src={astronaut2} alt="purple astronaut face" />
-          </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.button}>DOWNLOAD</button>
-          </div>
-        </div>
-        <div className={styles.pack}>
-          <h3>Judges</h3>
-          <h4>Judges Resources Pack</h4>
-          <div className={styles.imgContainer}>
-            <img src={astronaut3} alt="orange astronaut face" />
-          </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.button}>DOWNLOAD</button>
-          </div>
-        </div>
+        <Pack
+          title="Participants"
+          description="Participants Resources Pack"
+          img={<img src={astronaut1} alt="blue astronaut face" />}
+        />
+        <Pack
+          title="Mentors"
+          description="Mentors Resources Pack"
+          img={<img src={astronaut2} alt="purple astronaut face" />}
+        />
+        <Pack
+          title="Judges"
+          description="Judges Resources Pack"
+          img={<img src={astronaut3} alt="orange astronaut face" />}
+        />
       </div>
     </section>
   );
