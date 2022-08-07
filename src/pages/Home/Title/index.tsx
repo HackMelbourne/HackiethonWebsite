3c528244596237098bdd5748c415ade17093e9cc
Daniel <wengjae@gmail.com>
2022-08-07 22:23:11 +1000

    added buttons

--- src/pages/Home/Title/index.tsx
+++ src/pages/Home/Title/index.tsx
@@ -8,16 +8,6 @@ const Title = () => {
       <div className={styles.hero}>
         <Hero />
       </div>
-      <div className={styles.logo}>
-        <a
-          href="https://hackmelb.org"
-          title="HackMelbourne's website"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logo} alt="HackMelbourne Logo" />
-        </a>
-      </div>
     </section>
   );
 };
